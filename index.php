752512198318c00e2add4066f56425f3b21e3792
Bryan Egeland <bryqn292@gmail.com>
2025-01-31 14:46:41 +0700

    menambahkan paragraph dalam file index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit iste ea obcaecati, magni qui ipsam optio eum saepe dolorem repudiandae sunt, animi ullam alias, quibusdam quam at! Perspiciatis, ullam accusamus!</p>
 </body>
 </html>
